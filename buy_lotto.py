a2337abbd02b904d27474b685795a6b765eed9fa
sour12 <sour12@naver.com>
2023-09-19 22:39:57 +0900

    Update buy_lotto.py

--- buy_lotto.py
+++ buy_lotto.py
@@ -63,20 +63,17 @@ def run(playwright: Playwright) -> None:
     page.click("input:has-text(\"구매하기\")")
 
     time.sleep(2)
-try:
     # Click text=확인 취소 >> input[type="button"]
     page.click("text=확인 취소 >> input[type=\"button\"]")
-except Exception as e:
-    # 구매한도 넘었을때, 닫기 버튼
-    page.click("text=닫기")
-    context.close()
-    browser.close()
-    sys.exit()
-    
+
+try:
     # Click input[name="closeLayer"]
     page.click("input[name=\"closeLayer\"]")
     # assert page.url == "https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40"
-
+except Exception as e:
+    # 구매한도 넘었을때, 닫기 버튼
+    page.click("text=닫기")
+    
     # ---------------------
     context.close()
     browser.close()
