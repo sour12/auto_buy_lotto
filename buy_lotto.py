c9b9b4a3d6557e46ed5981c60790988a667d941a
sour12 <sour12@naver.com>
2023-09-21 11:53:21 +0900

    Update buy_lotto.py

--- buy_lotto.py
+++ buy_lotto.py
@@ -1,52 +1,41 @@
 from playwright.sync_api import Playwright, sync_playwright
 import time
 import sys
-import requests
 
 # 동행복권 아이디와 패스워드를 설정
 USER_ID = sys.argv[1]
 USER_PW = sys.argv[2]
+
+# 크롤링된 번호를 사용 여부 ("auto":미사용, "manual":사용)
 SEL_AUTO = sys.argv[3]
 
-def lotton_prediction():
-    # 총 횟수 확인 (23/9/19 기준 1085회까지 진행됨)
-    print("로또 현재 회차 찾기 ...")
-    num=1085
-    while True:
-        url="https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + str(num)
-        req=requests.get(url)
-        result=req.json()
-        if result.get("returnValue") == "fail":
-            break
-        num+=1
+FILE_PATH="./auto_lotto/weekly/count.log"
 
-    print("로또 번호 크롤링 ...")
-    count_num = {key: 0 for key in range(1, 46)}
-    count_bonus = {key: 0 for key in range(1, 46)}
-    for i in range(1, num):
-        url="https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo="+str(i)
-        req=requests.get(url)
-        result=req.json()
-        count_num[int(result["drwtNo1"])] += 1
-        count_num[int(result["drwtNo2"])] += 1
-        count_num[int(result["drwtNo3"])] += 1
-        count_num[int(result["drwtNo4"])] += 1
-        count_num[int(result["drwtNo5"])] += 1
-        count_num[int(result["drwtNo6"])] += 1
-        count_bonus[int(result["bnusNo"])] += 1
+count_num = {key: 0 for key in range(1, 46)}
+count_bonus = {key: 0 for key in range(1, 46)}
+combined_count = {key: 0 for key in range(1, 46)}
 
-    global top_6_keys
-    global bottom_6_keys
-    global combined_count
-    top_6_keys = sorted(count_num, key=lambda k: count_num[k], reverse=True)[:6]
-    bottom_6_keys = sorted(count_num, key=lambda k: count_num[k])[:6]
+def load_lotto_count():
+    rstr=""
+    with open(FILE_PATH, "r") as file:
+        rstr = file.read()
+
+    rstr_split=rstr.split("\n")    
+    count_num_str = rstr_split[1].split(';')
+    count_bonus_str = rstr_split[2].split(';')
+
+    idx=1
+    for val in count_num_str:
+        if (len(count_num_str) != idx):
+            count_num[idx]=int(val)
+        idx+=1
+    idx=1
+    for val in count_bonus_str:
+        if (len(count_bonus_str) != idx):
+            count_bonus[idx]=int(val)
+        idx+=1
+
     combined_count = {key: count_num[key] + count_bonus[key] for key in range(1, 46)}
-
-    print("<", (num-1), "> 회차 까지 !!!")    
-    print("가장 많이 뽑힌 번호:", sorted(top_6_keys))
-    print("가장 적게 뽑힌 번호:", sorted(bottom_6_keys))
-    print("[보너스포함] 가장 많이 뽑힌 번호:", sorted(sorted(combined_count, key=lambda k: combined_count[k], reverse=True)[:6]))
-    print("[보너스포함] 가장 적게 뽑힌 번호:", sorted(sorted(combined_count, key=lambda k: combined_count[k])[:6]))
 
 def manual_select(page, num_arr):
     print(num_arr)
@@ -78,10 +67,10 @@ def run(playwright: Playwright) -> None:
         page.click("text=혼합선택")
         page.select_option("select", str(1))
         # 4회는 번호선택
-        manual_select(page, (top_6_keys))
-        manual_select(page, (bottom_6_keys))
-        manual_select(page, sorted(combined_count, key=lambda k: combined_count[k])[:6])
-        manual_select(page, sorted(combined_count, key=lambda k: combined_count[k], reverse=True)[:6])
+        manual_select(page, (sorted(count_num, key=lambda k: count_num[k], reverse=True)[:6]))
+        manual_select(page, (sorted(count_num, key=lambda k: count_num[k])[:6]))
+        manual_select(page, (sorted(combined_count, key=lambda k: combined_count[k])[:6]))
+        manual_select(page, (sorted(combined_count, key=lambda k: combined_count[k], reverse=True)[:6]))
         # 1회는 자동선택
         page.click('label:has-text("자동선택")')
         page.click("text=확인")
@@ -104,7 +93,7 @@ def run(playwright: Playwright) -> None:
     browser.close()
 
 if (SEL_AUTO == "manual"):
-    lotton_prediction()
+    load_lotto_count()
 
 with sync_playwright() as playwright:
     run(playwright)
